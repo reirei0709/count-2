e37ff6fd5acc0cc47d7d94fa1035d3d527ff8997
mirei <usrmii301@gmail.com>
2020-09-09 12:32:58 +0900

    スキルチェック

--- app/src/main/java/app/itakura/reirei/count/MainActivity.kt
+++ app/src/main/java/app/itakura/reirei/count/MainActivity.kt
@@ -1,5 +1,6 @@
 package app.itakura.reirei.count
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +17,32 @@ class MainActivity : AppCompatActivity() {
 
             counter.text = count.toString()
 
+            if (count%3 == 0){
+                counter.setTextColor(Color.GREEN)
+            }else{
+                counter.setTextColor(Color.BLACK)
+            }
         }
+
+        minusButton.setOnClickListener {
+            count = count - 1
+
+            counter.text = count.toString()
+
+            if (count%3 == 0){
+                counter.setTextColor(Color.GREEN)
+            }else{
+                counter.setTextColor(Color.BLACK)
+            }
+
+
+        }
+
+        clearButton.setOnClickListener {
+            count = 0
+
+            counter.text = "0"
+        }
+
     }
 }
